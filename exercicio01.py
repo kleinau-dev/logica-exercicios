105d6783a21db935d344486018a8bab8bc38474a
Fernando Kleinau <fernandokleinau@gmail.com>
2025-06-30 21:37:49 -0300

    Exercícios 01 Python

---
+++ exercicio01.py
@@ -0,0 +1,15 @@
+#nome = "Fernando"
+#idade = 27
+#nome = input("Digite seu nome:")
+#idade = int(input("Digite sua idade"))
+
+#print("Olá", nome)
+
+a = int(input("Digite o primeiro número:"))
+b = int(input("Digite o segundo número:"))
+resultado = a + b 
+print("Resultado da soma:", resultado)
+
+nome = input("Digite seu nome:")
+idade = int(input("Digite sua idade:"))
+print(f"Olá, {nome}! Você tem {idade} anos.")
