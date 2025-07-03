4ddcea199d8c546734ca0451649e1e610bb02d73
Fernando Kleinau <fernandokleinau@gmail.com>
2025-07-02 21:34:43 -0300

    Atulização do exercicios-variaveis.py

--- exercicios-variaveis.py
+++ exercicios-variaveis.py
@@ -1,15 +1,15 @@
-#msg = "Olá, Mundo"
-#print(msg)
-#nome = input("Qual seu nome?")
-#print("Muito Prazer", nome)
-#idade = int(input ("Quantos ano você tem?"))
-#print("Caramba você é bem velho, então você tem", idade, "anos")
-#print("Que legal, prazer em conhece você", nome, "tenha um ótimo dia")
+msg = "Olá, Mundo"
+print(msg)
+nome = input("Qual seu nome?")
+print("Muito Prazer", nome)
+idade = int(input ("Quantos ano você tem?"))
+print("Caramba você é bem velho, então você tem", idade, "anos")
+print("Que legal, prazer em conhece você", nome, "tenha um ótimo dia")
 
-#a = int(input("Digite um número: "))
-#b = int(input("Digite outro número: "))
-#resultado = a + b / 2
-#print("A soma dos seus números e a média é", resultado)
+a = int(input("Digite um número: "))
+b = int(input("Digite outro número: "))
+resultado = a + b / 2
+print("A soma dos seus números e a média é", resultado)
 
-a = abs(-50)
+a = exp(-50)
 print(a)
